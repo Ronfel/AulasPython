eb4136eace36df9dab048b817f1683dbd00cb450
Ronfel <ronfelara@gmail.com>
2018-12-25 09:00:39 -0200

    Introdução ao Python

---
+++ aulas/aula1.py
@@ -0,0 +1,21 @@
+#comentário simples
+"""
+Comentário
+Multilinhas
+Python
+"""
+
+val_num = 10
+val_din = 7.3
+val_texto = "Qualquer Texto"
+
+print("O valor é:", val_num)
+print("O valor é: %i" %val_num)
+print("O valor é: " + str(val_num))
+
+print("\nO decimal é:", val_din)
+print("O decimal é: %.1f" %val_din)
+
+print("\nO texto é:", val_texto)
+print("O texto é: %s" %val_texto)
+print("O texto é: " + val_texto)
---
+++ aulas/aula2.py
@@ -0,0 +1,4 @@
+#entrada de Dados
+
+#Todo não finalizado
+login = input("Insira o valor 1:")
