b981642fbb3c62dcac68708311c5ffab56aea15d
Ronfel <ronfelara@gmail.com>
2021-03-04 19:14:27 -0300

    Implemetação de Metodo Estático.

---
+++ aulas/MetEstatico.py
@@ -0,0 +1,28 @@
+class MetEstatico:
+    def func1():
+        print("func1()")
+    def func2(x, y):
+        print("Função '{}' invocada. \nParams=({}, {})".format("func2", x, y))
+    def func3(a, b, c):
+        info = """
+        Nome da Função {nome}
+        Quantidade de Args {quantidade}
+        Args: {args}
+        """
+        info = info.format(nome=MetEstatico.func3.__name__,
+                    quantidade=MetEstatico.func3.__code__.co_argcount,
+                    args=MetEstatico.func3.__code__.co_varnames
+        )
+        print(info)
+
+    func1 = staticmethod(func1)
+    func2 = staticmethod(func2)
+    func3 = staticmethod(func3)
+    
+    
+me = MetEstatico
+me.func1()
+me.func2(700, 800)
+me.func3(300, 400, 500)
+
+
