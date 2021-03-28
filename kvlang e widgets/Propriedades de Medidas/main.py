4ae2c122969851513d3994a5a6e5eba9703d16b2
Ronfel <ronfelara@gmail.com>
2021-03-28 18:35:36 -0300

    Início dos estudos da biblioteca Kivy.

---
+++ kvlang e widgets/Propriedades de Medidas/main.py
@@ -0,0 +1,18 @@
+
+#author: Rodrigo Neves Fernandes Lara
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+
+
+class RootWidget(FloatLayout):
+    pass
+
+class MedidaApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MedidaApp().run()
---
+++ kvlang e widgets/hello-world-kvlang/main.py
@@ -0,0 +1,12 @@
+
+#author: Rodrigo Neves Fernandes Lara
+
+from kivy.app import App
+#from kivy.uix.label import Label
+
+class HelloApp(App):
+    pass
+    # def build(self):
+    #     return Label(text="Hello World!")
+
+HelloApp().run()
